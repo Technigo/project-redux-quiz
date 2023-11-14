4ddf5a0a5ccb4b164d049988e89ac0e2751725a1
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-11-14 15:21:07 +0100

    confetti

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
 import { quiz } from "../reducers/quiz";
+import Confetti from "react-confetti";
 
 const ResultScreen = () => {
   const dispatch = useDispatch();
@@ -7,12 +9,25 @@ const ResultScreen = () => {
   const correctAnswers = answers.filter((answer) => answer.isCorrect).length;
   const totalQuestions = useSelector((state) => state.quiz.questions.length);
 
+  const [confetti, setConfetti] = useState(false);
+
+  useEffect(() => {
+    setConfetti(true);
+
+    const timer = setTimeout(() => {
+      setConfetti(false);
+    }, 10000); // 10 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const restartQuiz = () => {
     dispatch(quiz.actions.restart());
   };
 
   return (
     <div>
+      {confetti && <Confetti />}
       <h1>Quiz Completed!</h1>
       <p>{`You got ${correctAnswers} out of ${totalQuestions} questions right.`}</p>
       <button onClick={restartQuiz}>Restart Quiz</button>
