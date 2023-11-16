4cad84979b930cfff048cd0ffc258649734f358d
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-16 23:50:21 +0100

    add comments to AnswerOptions.jsx

--- src/components/AnswerOptions.jsx
+++ src/components/AnswerOptions.jsx
@@ -9,20 +9,21 @@ export const AnswerOptions = () => {
   const dispatch = useDispatch();
 
   // state to save selected answer
-  const [ selectedAnswer, setSelectedAnswer ] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
   // state for showing correct answer
-  const [ showAnswer, setShowAnswer ] = useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   // separate answer options into array of separate strings
   const options = question.options.slice(0, 4);
 
+  // function to save selected answer and clear correct/incorrect message
   const handleChange = (e) => {
     setSelectedAnswer(parseInt(e.target.value));
     setShowAnswer(false);
   };
 
-  const handleClick = () => {
-    dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: selectedAnswer }));
+  // function to show if answer is correct
+  const handleCheck = () => {
     setShowAnswer(true);
   };
 
@@ -31,23 +32,38 @@ export const AnswerOptions = () => {
     isCorrect = "Correct!";
   } else {
     isCorrect = "Incorrect";
+  }
+
+  // function to submit answer, go to next question on clicking next btn
+  const handleNext = () => {
+    dispatch(
+      quiz.actions.submitAnswer({
+        questionId: question.id,
+        answerIndex: selectedAnswer,
+      })
+    );
+    dispatch(quiz.actions.goToNextQuestion());
+    setShowAnswer(false);
   };
 
   return (
     <div>
-        {options.map((option, index) => (
-          <div key={index} >
-            <input type="radio" name="option" id={index} value={index} onChange={handleChange}/>
-            <label htmlFor={index}>{option}</label>
-          </div>
-        ))}
-        <button onClick={handleClick}>Check Answer</button>
+      {options.map((option, index) => (
+        <div key={index}>
+          <input
+            type="radio"
+            name="option"
+            id={index}
+            value={index}
+            onChange={handleChange}
+          />
+          <label htmlFor={index}>{option}</label>
+        </div>
+      ))}
+      <button onClick={handleCheck}>Check Answer</button>
+      <button onClick={handleNext}>Next</button>
 
-        {showAnswer && (
-          <p>
-            {isCorrect}
-          </p>
-        )}
+      {showAnswer && <p>{isCorrect}</p>}
     </div>
   );
 };
