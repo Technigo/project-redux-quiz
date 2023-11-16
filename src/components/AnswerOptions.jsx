bf1248dc8110d7c1d4c928d3e8ec268132cbd102
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-16 23:17:24 +0100

    show correct or incorrect

--- src/components/AnswerOptions.jsx
+++ src/components/AnswerOptions.jsx
@@ -9,17 +9,28 @@ export const AnswerOptions = () => {
   const dispatch = useDispatch();
 
   // state to save selected answer
-  const [ selectedAnswer, setSelectedAnswer ] = useState("");
+  const [ selectedAnswer, setSelectedAnswer ] = useState(null);
+  // state for showing correct answer
+  const [ showAnswer, setShowAnswer ] = useState(false);
 
   // separate answer options into array of separate strings
   const options = question.options.slice(0, 4);
 
   const handleChange = (e) => {
     setSelectedAnswer(parseInt(e.target.value));
+    setShowAnswer(false);
   };
 
   const handleClick = () => {
     dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: selectedAnswer }));
+    setShowAnswer(true);
+  };
+
+  let isCorrect;
+  if (selectedAnswer === question.correctAnswerIndex) {
+    isCorrect = "Correct!";
+  } else {
+    isCorrect = "Incorrect";
   };
 
   return (
@@ -30,7 +41,13 @@ export const AnswerOptions = () => {
             <label htmlFor={index}>{option}</label>
           </div>
         ))}
-        <button onClick={handleClick}>Next</button>
+        <button onClick={handleClick}>Check Answer</button>
+
+        {showAnswer && (
+          <p>
+            {isCorrect}
+          </p>
+        )}
     </div>
   );
 };
