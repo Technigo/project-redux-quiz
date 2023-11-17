a5b587bb1a328211bd399a86d333c3ef7f84ee59
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-17 10:25:57 +0100

    change styling of show correct answer

--- src/components/AnswerOptions.jsx
+++ src/components/AnswerOptions.jsx
@@ -1,38 +1,37 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { quiz } from "../reducers/quiz";
+import "./AnswerOptions.css";
 
 export const AnswerOptions = () => {
+  // get global state
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
+
   const dispatch = useDispatch();
 
   // state to save selected answer
   const [selectedAnswer, setSelectedAnswer] = useState("");
+
   // state for showing correct answer
   const [showAnswer, setShowAnswer] = useState(false);
 
   // separate answer options into array of separate strings
   const options = question.options.slice(0, 4);
 
+  // array of letters for multiple choice
+  const letters = ["A. ", "B. ", "C. ", "D. "];
+
   // function to save selected answer and clear correct/incorrect message
-  const handleChange = (e) => {
-    setSelectedAnswer(parseInt(e.target.value));
-    setShowAnswer(false);
+  const handleClick = (e) => {
+    setSelectedAnswer(e.target.value);
   };
 
   // function to show if answer is correct
   const handleCheck = () => {
     setShowAnswer(true);
   };
-
-  let isCorrect;
-  if (selectedAnswer === question.correctAnswerIndex) {
-    isCorrect = "Correct!";
-  } else {
-    isCorrect = "Incorrect";
-  }
 
   // function to submit answer, go to next question on clicking next btn
   const handleNext = () => {
@@ -47,23 +46,23 @@ export const AnswerOptions = () => {
   };
 
   return (
-    <div>
+    <div className="multiChoiceContainer">
       {options.map((option, index) => (
         <div key={index}>
-          <input
-            type="radio"
-            name="option"
-            id={index}
-            value={index}
-            onChange={handleChange}
-          />
-          <label htmlFor={index}>{option}</label>
+          <button
+            type="button"
+            className={showAnswer && question.correctAnswerIndex === index ? "correct multiChoice" : "multiChoice"}
+            name={option}
+            value={option}
+            style={{ backgroundColor: selectedAnswer === option ? "pink" : null }}
+            onClick={handleClick}
+          >
+            {letters[index]}{option}
+          </button>
         </div>
       ))}
       <button onClick={handleCheck}>Check Answer</button>
       <button onClick={handleNext}>Next</button>
-
-      {showAnswer && <p>{isCorrect}</p>}
     </div>
   );
 };
