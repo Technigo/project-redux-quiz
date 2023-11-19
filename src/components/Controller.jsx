9252f430742ad025787a2fc71557dcc2e08c5087
May Sunktong <pattaranan.sunktong@gmail.com>
2023-11-19 22:11:10 +0100

    Styling QuizSummary, create a modal for QuizSummary

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,25 +1,42 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { useSelector } from 'react-redux';
-import { ButtonsMenu } from "./ButtonsMenu";
+import { ButtonsMenu } from './ButtonsMenu';
 import { CurrentQuestion } from './CurrentQuestion';
-import { Header } from "./Header";
+import { Header } from './Header';
 import ProgressBar from './ProgressBar';
 import { QuizSummary } from './QuizSummary';
 
 export const Controller = () => {
-  const quiz= useSelector((state) => state.quiz);
-  console.log("QUizzzzz", quiz)
-  const isQuizOver = quiz.quizOver
+  const quiz = useSelector((state) => state.quiz);
+  const [isFinishModalOpen, setFinishModalOpen] = useState(false);
 
-  if (isQuizOver) {
-    return <QuizSummary />
-  }
+  const isQuizOver = quiz.quizOver;
+
+  const handleShowFinishModal = () => {
+    setFinishModalOpen(true);
+  };
+
+  const handleCloseFinishModal = () => {
+    setFinishModalOpen(false);
+  };
 
   return (
     <div className="controller-container">
-        <Header/>
-        <CurrentQuestion />
-        <ProgressBar/>
-        <ButtonsMenu/>
+      <Header />
+      <CurrentQuestion />
+      <ProgressBar />
+      <ButtonsMenu onShowFinishModal={handleShowFinishModal} />
+      <Modal
+        isOpen={isFinishModalOpen}
+        onRequestClose={handleCloseFinishModal}
+        contentLabel="Quiz Finish"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <QuizSummary />
+        <button onClick={handleCloseFinishModal}>Close</button>
+      </Modal>
     </div>
-  )
-}
+  );
+};
