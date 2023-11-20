06cbcda10f751d77e085090bd37a0d7a44e5b197
Emma Corazza <emma0282@hotmail.com>
2023-11-20 20:41:45 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { quiz } from './reducers/quiz';
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { quiz } from "./reducers/quiz";
 
-import { CurrentQuestion } from './components/CurrentQuestion';
+import { CurrentQuestion } from "./components/CurrentQuestion";
 
 const reducer = combineReducers({
-  quiz: quiz.reducer
+  quiz: quiz.reducer,
 });
 
 const store = configureStore({ reducer });
@@ -17,4 +16,4 @@ export const App = () => {
       <CurrentQuestion />
     </Provider>
   );
-}
+};
--- src/components/CurrentQuestion.jsx
+++ src/components/CurrentQuestion.jsx
@@ -1,18 +1,69 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { goToNextQuestion, submitAnswer } from "../reducers/quiz";
+import { Summary } from "./Summary";
 
 export const CurrentQuestion = () => {
-  const question = useSelector(
-    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
+  const dispatch = useDispatch();
+  const questions = useSelector((state) => state.quiz.questions);
+  const currentQuestionIndex = useSelector(
+    (state) => state.quiz.currentQuestionIndex
   );
+  const question = questions[currentQuestionIndex];
+
+  const isQuizOver = useSelector((state) => state.quiz.quizOver);
+
+  const handleAnswer = (answerIndex) => {
+    if (question.correctAnswerIndex === answerIndex) {
+      setIsCorrectAnswer(true);
+    } else {
+      setIsCorrectAnswer(false);
+    }
+
+    setTimeout(async () => {
+      setSelectedAnswerIndex(null);
+      dispatch(submitAnswer({ questionId: question.id, answerIndex }));
+      dispatch(goToNextQuestion());
+    }, 1500);
+  };
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
 
+  if (isQuizOver) {
+    return <Summary />;
+  }
   return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
+    <div className="wrapper">
+      <h1 className="question">{question.questionText}</h1>
+      <ul>
+        {question.options.map((option, index) => (
+          <li key={index} className="answers">
+            <button
+              className={
+                index === selectedAnswerIndex
+                  ? isCorrectAnswer
+                    ? "buttonCorrect"
+                    : "buttonWrong"
+                  : "buttonChoise"
+              }
+              onClick={() => {
+                setSelectedAnswerIndex(index);
+                handleAnswer(index);
+              }}
+              disabled={isQuizOver}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p className="questionLeft">
+        Question number: {currentQuestionIndex + 1} out of {questions.length}
+      </p>
     </div>
   );
 };
