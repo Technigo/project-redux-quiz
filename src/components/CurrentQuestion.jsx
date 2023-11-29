8862d6e9f280a5ec9dd2acb45d2f1d53c6e5c05f
BEHROUZ RASOULNIA <112555779+Behrouz87RN@users.noreply.github.com>
2023-11-30 01:37:26 +0330

    debug next question

--- src/components/CurrentQuestion.jsx
+++ src/components/CurrentQuestion.jsx
@@ -31,7 +31,7 @@ export const CurrentQuestion = () => {
 
   const handleAnswerClick = (answerIndex, questionId) => {
     dispatch(submitAnswer({ answerIndex, questionId }));
-    dispatch(goToNextQuestion());
+    // dispatch(goToNextQuestion());
   };
 
   const withImage = 'optionsImages' in question;
